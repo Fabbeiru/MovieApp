072d4237df1623c5ce420339c48c799976580c04
Fabian B <fabian@secret-source.eu>
2022-04-11 23:56:45 +0100

    Correctly fetching data from the API

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './blackLogo.png';
 
+const apiKey : string = "9fdb091c";
+
 function App() {
+
+  const [movies, setMovies] = useState();
+
+  useEffect(() => {
+    fetch("http://www.omdbapi.com/?i=tt3896198&apikey=" + apiKey)
+    .then(response => {
+      return response.json()
+    })
+    .then(data => {
+      console.log(data);
+      setMovies(data);
+    });
+
+    /* const getMovies = async () => {
+      const response = await fetch("http://www.omdbapi.com/?i=tt3896198&apikey=" + apiKey);
+      const body = await response.json();
+      setBody(body);
+    }
+
+    getMovies(); */
+    
+  }, []);
+
   return (
     <div className="App">
       <div className="wrapper">
@@ -11,8 +36,7 @@ function App() {
         </header>
 
         <form className='input-form' >
-            <input type="text" placeholder='Search a movie by title' autoFocus
-              />
+            <input type="text" placeholder='Search a movie by title' autoFocus/>
             <button type="submit" className='input-button' title="Create task">Search</button>
         </form>
 
