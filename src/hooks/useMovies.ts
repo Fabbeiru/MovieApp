fe55f676cc707ae63488b7c4c0f2e2c60affa86e
Jerome Gill <jerome.gill@s4rb.com>
2022-05-03 12:24:50 +0100

    Do not allow getMovies when awaiting another call

--- src/hooks/useMovies.ts
+++ src/hooks/useMovies.ts
@@ -1,62 +1,66 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from "react"
 
-const apiKey : string = "9fdb091c";
+const apiKey: string = "9fdb091c"
 
 export interface MovieById {
-    Title: string,
-    Year: string,
-    Runtime: string,
-    Plot: string,
-    imdbRating: string,
-    Poster: string
+  Title: string
+  Year: string
+  Runtime: string
+  Plot: string
+  imdbRating: string
+  Poster: string
 }
-  
+
 export interface MovieByTitle {
-    Title: string,
-    imdbID: string,
-    Type: string,
-    Poster: string
+  Title: string
+  imdbID: string
+  Type: string
+  Poster: string
 }
 
 export function useMovies(query: string, searchById: boolean) {
+  const [result, setResult] = useState<any>()
+  const [loading, setLoading] = useState<boolean>(false)
 
-    const [result, setResult] = useState<any>();
-    const [loading, setLoading] = useState<boolean>(false);
+  const getMovies = async () => {
+    try {
+      setLoading(true)
+      const response = await fetch(
+        "https://www.omdbapi.com/?" +
+          (searchById ? "i=" : "s=") +
+          query +
+          "&apikey=" +
+          apiKey
+      )
+      if (!response.ok) {
+        throw new Error()
+      }
+      const data = await response.json()
+      if (data.Response === "False") {
+        alert("Ooops! " + data.Error)
+        return
+      }
+      if (data === undefined) {
+        alert("Ooops! Please try again")
+        return
+      }
+      console.log(data)
+      setResult(data)
+      setLoading(false)
+    } catch (error) {
+      console.log(error)
+      alert(
+        "Something went wrong while connecting to the API. Please check spelling"
+      )
+    }
+  }
 
-    useEffect(() => {
+  useEffect(() => {
+    if (query === "") return
+    if (loading === false) {
+      getMovies()
+    }
+  }, [query, loading])
 
-        if (query === "")  return;
-
-        setLoading(true);
-        const getMovies = async() => {
-            try {
-                const response = await fetch("https://www.omdbapi.com/?" + (searchById ? "i=" : "s=") + query + "&apikey=" + apiKey);
-                if (!response.ok) {
-                    throw new Error();
-                }
-                const data = await response.json();
-                if (data.Response === "False") {
-                    alert("Ooops! " + data.Error);
-                    return;
-                }
-                if (data === undefined) {
-                    alert("Ooops! Please try again");
-                    return;
-                }
-                console.log(data)
-                setResult(data);
-                console.log(result);
-
-            } catch (error) {
-                console.log(error);
-                alert("Something went wrong while connecting to the API. Please check spelling");
-            }
-        }
-
-        getMovies();
-        setLoading(false);
-
-    }, [query])
-
-    return {result, loading};
+  return { result, loading }
 }
