59e15d3c32f6f49dc8f3f35755c8bde1760f248a
Fabian B <fabian@secret-source.eu>
2022-05-08 16:58:54 +0100

    Updated @types/react and react-dom. Also using new createRoot

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
-ReactDOM.render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
